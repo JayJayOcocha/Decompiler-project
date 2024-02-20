ef3db25cd0f23ff0ac6e18cdfa0d460c20e3b81e
JayJayOcocha <160603569+JayJayOcocha@users.noreply.github.com>
2024-02-20 21:54:02 +0900

    초기 프로젝트 (맵 하나 만들어져 있음)

---
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+// 기본 적으로 게임 내의 Script는 MonoBehaviour 라는 클래스를 상속받는다.
+// MonoBehaviour란? 게임 로직 구성에 필요한 모든 함수, 속성 등을 포함한다.
+{
+    public Vector2 inputVec;
+    public float speed;
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid =  GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // 물리 연산 프레임마다 호출되는 생명주기 함수
+    void FixedUpdate()
+    {
+        // // 1. 힘을 준다.
+        // rigid.AddForce(inputVec);
+
+        // // 2. 속도 제어
+        // rigid.velocity = inputVec;
+
+        // 3. 위치 이동
+        // MovePosition 은 위치 이동이라 현재 위치도 더해주어야 함
+
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;  
+        // normalized는 어느 방향이던지 1의 크기로 정해준다.
+        // speed는 그냥 전역변수 필드에서 정해준 변수이고
+        // Time.fixedDeltaTime 는 fixedUpdate 한 번 만큼 / 물리 프레임 한 번 만큼 소모된 시간. 왜 필요한 거지?
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+}
